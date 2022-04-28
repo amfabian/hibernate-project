f1c836bdeb57ab094223255e6f3c7ed82893a36a
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-28 09:47:33 -0300

    Set e Get classe User

--- src/main/java/dev/ifrs/model/User.java
+++ src/main/java/dev/ifrs/model/User.java
@@ -1,5 +1,17 @@
 package dev.ifrs.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class User {
+    private String name;
+
+   
+    public String getName(){
+        return name;
+    }
     
+    public void setName(String name) {
+        this.name = name;
+    }
 }
