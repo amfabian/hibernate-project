c187b52e98c171452ca45909f8be08cd45a4452f
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-28 09:40:54 -0300

    Criado classe User e createUser em Service

--- src/main/java/dev/ifrs/Service.java
+++ src/main/java/dev/ifrs/Service.java
@@ -1,6 +1,24 @@
 package dev.ifrs;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import dev.ifrs.model.User;
 
+@Path("/api")
 public class Service {
+
+    @GET
+    @Path("/user/create/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+        public User creatUser(@PathParam("name") String name) {
+
+            User user = new User();
+            user.setName(name);
+            return user;
+        }
+
     
 }
