032d826f164ac441ac509918ca548b3e9ad213fb
Alexandre Fabian <xandy.fabian@gmail.com>
2022-05-05 09:48:43 -0300

    Classe Channel criada e configurada

--- src/main/java/dev/ifrs/model/Channel.java
+++ src/main/java/dev/ifrs/model/Channel.java
@@ -1,5 +1,10 @@
 package dev.ifrs.model;
 
-public class Channel {
-    
-}
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity @Getter @Setter
+public class Channel extends PanacheEntity {private String hash;}
