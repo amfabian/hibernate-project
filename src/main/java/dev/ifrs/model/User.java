b843d8162e758b6a8c5f8d8df0c0d0e99d0cc551
Alexandre Fabian <xandy.fabian@gmail.com>
2022-05-05 08:57:53 -0300

    implementado relacionamento User Message

--- src/main/java/dev/ifrs/model/User.java
+++ src/main/java/dev/ifrs/model/User.java
@@ -1,6 +1,12 @@
 package dev.ifrs.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
@@ -8,7 +14,11 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 public class User extends PanacheEntity{
     private String name;
 
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private List<Message> messages;
    
+
     public String getName(){
         return name;
     }
