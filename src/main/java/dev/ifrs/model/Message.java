bee203d722418c996bdb7f15139c767b2e980cf1
Alexandre Fabian <xandy.fabian@gmail.com>
2022-05-05 09:27:50 -0300

    add support lombok

--- src/main/java/dev/ifrs/model/Message.java
+++ src/main/java/dev/ifrs/model/Message.java
@@ -3,18 +3,21 @@ package dev.ifrs.model;
 import javax.persistence.Entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter @Getter
 public class Message extends PanacheEntity{
     private String text;
 
-    public String getText() {
+  /*   public String getText() {
         return text;
     }
 
     public void setText(String text) {
         this.text = text;
-    }
+    } */
 
     
 }
