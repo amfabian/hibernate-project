e457db9b376fe82b0cda59c717aa9f8557371476
Alexandre Fabian <xandy.fabian@gmail.com>
2022-05-17 18:01:05 -0300

    criado metodo add

--- src/main/java/dev/ifrs/ChannelWS.java
+++ src/main/java/dev/ifrs/ChannelWS.java
@@ -1,6 +1,7 @@
 package dev.ifrs;
 
 import javax.transaction.Transactional;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,6 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import dev.ifrs.model.Channel;
+import dev.ifrs.model.User;
 
 @Path("/channel")
 @Transactional
@@ -23,6 +25,27 @@ public class ChannelWS {
       channel.setHash(hash);
       channel.persist();
       return channel;
+   }
+
+   @GET
+   @Path("/add/{idChannel}/{idUser}")
+   @Produces(MediaType.APPLICATION_JSON)
+   public User add(@PathParam("idChannel") Long idChannel, @PathParam("idUser") Long idUser) {
+
+      Channel channel = Channel.findById(idChannel);
+      if (channel == null)
+         throw new BadRequestException("Channel not found");
+
+      User user = User.findById(idUser);
+      if (user == null)
+         throw new BadRequestException("User not found");
+
+      channel.addUser(user);
+      user.addChannel(channel);
+
+      user.persist();
+
+      return user;
    }
 
     
