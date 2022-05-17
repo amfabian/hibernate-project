51fb409487a230e01843437d08b5e0953e4c6a23
Alexandre Fabian <xandy.fabian@gmail.com>
2022-05-17 17:56:20 -0300

    add atributos na anotacao ManyToMany

--- src/main/java/dev/ifrs/model/Channel.java
+++ src/main/java/dev/ifrs/model/Channel.java
@@ -2,18 +2,22 @@ package dev.ifrs.model;
 
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity @Getter @Setter
+@Entity @Getter @Setter @NoArgsConstructor
 public class Channel extends PanacheEntity {
     private String hash;
     
-    @ManyToMany
+    @ManyToMany(mappedBy = "channels", fetch = FetchType.EAGER)
      private List<User> users;
 
-  
+     public void addUser(User user) {
+        this.users.add(user);
+    }
 }
 
